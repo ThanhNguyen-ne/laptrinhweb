8e12990e15fdc08f391818671dd7622e8bc03479
Nguyễn Quốc Tùng <2251120259@ut.edu.vn>
2024-08-13 09:20:04 +0700

    header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,9 +1,12 @@
+// Không ẩn header khi cuộn
+const header = document.querySelector("header");
+
 window.addEventListener("scroll", function () {
-    var header = document.querySelector(".header_duoi");
-    var headerPosition = header.offsetTop;
-    if (window.pageYOffset > headerPosition) {
-        header.classList.add("fixed");
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > 100) {
+        header.style.backgroundColor = "rgba(173, 216, 230, 0.9)"; // Thay đổi màu nền header khi cuộn xuống
     } else {
-        header.classList.remove("fixed");
+        header.style.backgroundColor = "rgb(173, 216, 230)"; // Khôi phục màu nền ban đầu khi quay lại đầu trang
     }
 });
