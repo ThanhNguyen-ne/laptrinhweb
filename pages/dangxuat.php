e532da6be248f9a6bec109942e192aa48e7c32a2
your name <2251120246@ut.edu.vn>
2024-08-27 15:25:35 +0700

    xong đăng nhập

--- pages/dangxuat.php
+++ pages/dangxuat.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+
+// Lấy đường dẫn trang hiện tại
+$current_page = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'index.php';
+
+// Hủy session
 session_destroy();
-header('Location: index.php');
+
+// Chuyển hướng về trang trước đó hoặc `index.php` nếu không có trang trước đó
+header("Location: $current_page");
 exit();
